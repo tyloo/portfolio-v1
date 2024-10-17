412a62581bfa407056cdf476e1b82acc079536bc
tyloo <jbonva@gmail.com>
2024-10-17 16:44:23 +0200

    chore: added recent posts
    Signed-off-by: tyloo <jbonva@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Intro from '@/components/sections/intro'
+import RecentPosts from '@/components/sections/recent-posts'
 
 export default async function Home() {
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
         <Intro />
+
+        <RecentPosts />
 
         <div className='min-w-full'>
           <h1 className='text-center font-mono text-3xl text-blue-500 text-muted-foreground lg:text-6xl'>
---
+++ src/components/sections/recent-posts.tsx
@@ -0,0 +1,23 @@
+import { getPosts } from '@/lib/posts'
+import Posts from './posts'
+import Link from 'next/link'
+
+export default async function RecentPosts() {
+  const posts = await getPosts(4)
+
+  return (
+    <section className='pb-24'>
+      <div>
+        <h2 className='title mb-12'>Recent posts</h2>
+        <Posts posts={posts} />
+
+        <Link
+          href='/posts'
+          className='mt-8 inline-flex items-center gap-2 text-muted-foreground underline decoration-1 underline-offset-2 transition-colors hover:text-foreground'
+        >
+          <span>All posts</span>
+        </Link>
+      </div>
+    </section>
+  )
+}
