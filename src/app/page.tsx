5a25c7b9a7d9cd8b75569a4bcb3c7e0f86b33cc7
tyloo <jbonva@gmail.com>
2024-10-17 17:20:45 +0200

    chore: added not found page
    Signed-off-by: tyloo <jbonva@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Intro from '@/components/sections/intro'
 import RecentPosts from '@/components/sections/recent-posts'
 import RecentProjects from '@/components/sections/recent-projects'
 
-export default async function Home() {
+export default async function HomePage() {
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
